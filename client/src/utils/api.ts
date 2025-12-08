5d60baede82ec6b7b15f69511a133a371eacbfea
Equinoxic <sorasakihoshi@gmail.com>
2025-12-08 19:31:00 +0700

    edit

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://projectwebserver-production.up.railway.app/api',
+    baseURL: 'https://projectwebserver-production.up.railway.app/api',
 
     // api when running locally from docker
-    baseURL: 'http://localhost:5001/api',
+    // baseURL: 'http://localhost:5001/api',
 
     headers: {
         'Content-Type': 'application/json',
